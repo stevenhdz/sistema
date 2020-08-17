17a26f6819052f1a5528150cad1ac8e9d9f5396c
Alexander Jimenez <alexanderjimenez@MacBook-Pro.local>
2020-08-17 15:47:45 -0500

    add2
    add2

---
+++ modelos/Consultas.php
@@ -0,0 +1,38 @@
+<?php 
+//Incluímos inicialmente la conexión a la base de datos
+require "../config/conexion.php";
+
+Class Consultas
+{
+	//Implementamos nuestro constructor
+	public function __construct()
+	{
+
+	}
+
+	public function comprasfecha($fecha_inicio,$fecha_fin)
+	{
+		$sql="SELECT DATE(i.fecha_hora) as fecha,u.nombre as usuario, p.nombre as proveedor,i.tipo_comprobante,i.serie_comprobante,i.num_comprobante,i.total_compra,i.impuesto,i.estado FROM ingreso i INNER JOIN persona p ON i.idproveedor=p.idpersona INNER JOIN usuario u ON i.idusuario=u.idusuario WHERE DATE(i.fecha_hora)>='$fecha_inicio' AND DATE(i.fecha_hora)<='$fecha_fin'";
+		return ejecutarConsulta($sql);		
+	}
+
+	public function ventasfechacliente($fecha_inicio,$fecha_fin,$idcliente)
+	{
+		$sql="SELECT DATE(v.fecha_hora) as fecha,u.nombre as usuario, p.nombre as cliente,v.tipo_comprobante,v.serie_comprobante,v.num_comprobante,v.total_venta,v.impuesto,v.estado FROM venta v INNER JOIN persona p ON v.idcliente=p.idpersona INNER JOIN usuario u ON v.idusuario=u.idusuario WHERE DATE(v.fecha_hora)>='$fecha_inicio' AND DATE(v.fecha_hora)<='$fecha_fin' AND v.idcliente='$idcliente'";
+		return ejecutarConsulta($sql);		
+	}
+
+	public function totalcomprahoy()
+	{
+		$sql="SELECT IFNULL(SUM(total_compra),0) as total_compra FROM ingreso WHERE DATE(fecha_hora)=now()";
+		return ejecutarConsulta($sql);
+	}
+
+	public function totalventahoy()
+	{
+		$sql="SELECT IFNULL(SUM(total_venta),0) as total_venta FROM venta WHERE DATE(fecha_hora)=now()";
+		return ejecutarConsulta($sql);
+	}
+}
+
+?>
--- vistas/header.php
+++ vistas/header.php
@@ -188,7 +188,7 @@ if(strlen(session_id())< 1)
                       <i class="fa fa-angle-left pull-right"></i>
                     </a>
                     <ul class="treeview-menu">
-                      <li><a href="consultacompras.php"><i class="fa fa-circle-o"></i> Consulta Compras</a></li>                
+                      <li><a href="comprasfecha.php"><i class="fa fa-circle-o"></i> Consulta Compras</a></li>                
                     </ul>
                   </li>';
                 }
@@ -202,7 +202,7 @@ if(strlen(session_id())< 1)
                       <i class="fa fa-angle-left pull-right"></i>
                     </a>
                     <ul class="treeview-menu">
-                      <li><a href="consultaventas.php"><i class="fa fa-circle-o"></i> Consulta Ventas</a></li>                
+                      <li><a href="ventasfechacliente.php"><i class="fa fa-circle-o"></i> Consulta Ventas</a></li>                
                     </ul>
                   </li>';
                 }
